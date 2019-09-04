fd84db41d9bbcbff602431925d7fbb6df0be502c
wanglei <cn_wlei@163.com>
2019-09-04 16:56:46 +0800

    新增：运营商管理后台-模板管理-增删改查操作

--- pinyougou-manager-web/src/main/webapp/js/controller/typeTemplateController.js
+++ pinyougou-manager-web/src/main/webapp/js/controller/typeTemplateController.js
@@ -26,7 +26,10 @@ app.controller('typeTemplateController' ,function($scope,$controller,typeTemplat
 	$scope.findOne=function(id){				
 		typeTemplateService.findOne(id).success(
 			function(response){
-				$scope.entity= response;					
+				$scope.entity= response;
+				$scope.entity.brandIds=  JSON.parse($scope.entity.brandIds);//转换品牌列表
+				$scope.entity.specIds=  JSON.parse($scope.entity.specIds);//转换规格列表
+				$scope.entity.customAttributeItems= JSON.parse($scope.entity.customAttributeItems);//转换扩展属性
 			}
 		);				
 	}
@@ -98,5 +101,15 @@ app.controller('typeTemplateController' ,function($scope,$controller,typeTemplat
 			function(response){
 				$scope.specList={data:response};
 			});
+	}
+
+	//新增扩展属性行
+	$scope.addTableRow=function(){
+		$scope.entity.customAttributeItems.push({});
+	}
+
+	//删除扩展属性行
+	$scope.deleTableRow=function (index) {
+		$scope.entity.customAttributeItems.splice(index,1);
 	}
 });	
