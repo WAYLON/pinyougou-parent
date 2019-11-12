2e403fa32fe3211f6a718e849b824ddf569adc8e
wanglei-007 <804818167@qq.com>
2019-11-12 23:34:26 +0800

    新增：SpringBoot框架与短信解决方案(腾讯云+redis+activeMQ)

---
+++ pinyougou-user-web/src/main/webapp/js/base.js
@@ -0,0 +1,7 @@
+var app=angular.module('pinyougou',[]);
+/*$sce服务写成过滤器*/
+app.filter('trustHtml',['$sce',function($sce){
+    return function(data){
+        return $sce.trustAsHtml(data);
+    }
+}]);
---
+++ pinyougou-user-web/src/main/webapp/js/controller/userController.js
@@ -0,0 +1,29 @@
+//控制层
+app.controller('userController' ,function($scope,$controller   ,userService){
+	//注册
+	$scope.reg=function(){
+		if($scope.entity.password!=$scope.password)  {
+			alert("两次输入的密码不一致，请重新输入");
+			return ;
+		}
+		userService.add( $scope.entity  ).success(
+			function(response){
+				alert(response.message);
+			}
+		);
+	}
+	//发送验证码
+	$scope.sendCode=function(){
+		if($scope.entity.phone==null){
+			alert("请输入手机号！");
+			return ;
+		}
+		userService.sendCode($scope.entity.phone).success(
+			function(response){
+				alert(response.message);
+			}
+		);
+	}
+
+
+});
